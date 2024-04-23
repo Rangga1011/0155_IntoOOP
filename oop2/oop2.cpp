15667792308ac0a912414a66a9e7d591a4c0ec84
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-04-23 13:00:04 +0700

    Menambahkan input class bangun datar

--- oop2/oop2.cpp
+++ oop2/oop2.cpp
@@ -1,58 +1,12 @@
 #include <iostream>
 using namespace std;
 
-class BangunDatar {
+class bangunDatar {
 private:
-    // akses modifier
-    float panjang, lebar;
-
+	float panjang, lebar;
 public:
-    // Constructor
-    BangunDatar() {}
-
-    // Setter
-    void setPanjang(float panjang) {
-        this->panjang = panjang;
-    }
-
-    void setLebar(float lebar) {
-        this->lebar = lebar;
-    }
-
-    // Getter
-    float getPanjang() const {
-        return panjang;
-    }
-
-    float getLebar() const {
-        return lebar;
-    }
-
-    // Hitung luas
-    float hitungLuas() const {
-        return panjang * lebar;
-    }
-
-    // Tampilkan informasi
-    void display() const {
-        cout << "Panjang: " << panjang << endl;
-        cout << "Lebar: " << lebar << endl;
-        cout << "Luas: " << hitungLuas() << endl;
-    }
+	float luas;
 };
-
-int main() {
-    BangunDatar bangunDatar;
-
-    // Set nilai panjang dan lebar
-    bangunDatar.setPanjang(10);
-    bangunDatar.setLebar(5);
-
-    // Tampilkan informasi
-    bangunDatar.display();
-
-    return 0;
-}
 
 
 
