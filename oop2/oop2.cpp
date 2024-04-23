d9922d9c3245325b8f1add73b79ed033aee38de8
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-04-23 13:03:01 +0700

    Menambahkan void input bangun datar

--- oop2/oop2.cpp
+++ oop2/oop2.cpp
@@ -6,6 +6,14 @@ private:
 	float panjang, lebar;
 public:
 	float luas;
+
+	void input() { // metode input persegi panjang
+
+		cout << "Masukan Panjangnya = ";
+		cin >> panjang;
+		cout << "Masukan Lebarnya = ";
+		cin >> lebar;
+	}
 };
 
 
