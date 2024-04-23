46d39f50f48220cc0d820aa847d5c8a68ab1f894
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-04-23 10:25:53 +0700

    Menambahkan  class Bangun datar

---
+++ oop2/oop2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class BangunDatar {
+private:
+    // akses modifier
+    float panjang, lebar;
+
+public:
+    // Constructor
+    BangunDatar() {}
+
+    // Setter
+    void setPanjang(float panjang) {
+        this->panjang = panjang;
+    }
+
+    void setLebar(float lebar) {
+        this->lebar = lebar;
+    }
+
+    // Getter
+    float getPanjang() const {
+        return panjang;
+    }
+
+    float getLebar() const {
+        return lebar;
+    }
+
+    // Hitung luas
+    float hitungLuas() const {
+        return panjang * lebar;
+    }
+
+    // Tampilkan informasi
+    void display() const {
+        cout << "Panjang: " << panjang << endl;
+        cout << "Lebar: " << lebar << endl;
+        cout << "Luas: " << hitungLuas() << endl;
+    }
+};
+
+
+
