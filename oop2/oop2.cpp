394a1cc23f5e4c65bf04a823e3032a2b3dbf8774
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-04-23 10:27:35 +0700

    Menambahkan int main bangun datar

--- oop2/oop2.cpp
+++ oop2/oop2.cpp
@@ -41,5 +41,18 @@ public:
     }
 };
 
+int main() {
+    BangunDatar bangunDatar;
+
+    // Set nilai panjang dan lebar
+    bangunDatar.setPanjang(10);
+    bangunDatar.setLebar(5);
+
+    // Tampilkan informasi
+    bangunDatar.display();
+
+    return 0;
+}
+
 
 
